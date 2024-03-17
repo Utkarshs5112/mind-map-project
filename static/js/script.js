dda1279473a48d786601eaf0c413c5f910e15ece
Ravikant Munda <126383121+raven784@users.noreply.github.com>
2024-03-17 21:40:39 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -30,3 +30,28 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+// Function to fetch data from clues.json served by Flask
+async function fetchData() {
+    try {
+        const response = await fetch('clues.json');
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+}
+
+async function populateTable() {
+    const jsonData = await fetchData();
+    const tableBody = document.getElementById("table-body");
+    jsonData.clues.forEach(clue => {
+        const row = tableBody.insertRow();
+        row.insertCell(0).textContent = clue.clue;
+        row.insertCell(1).textContent = clue.date;
+        row.insertCell(2).textContent = clue.category;
+    });
+}
+
+populateTable();
+
+
